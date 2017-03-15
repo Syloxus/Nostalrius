10d753c4b5dd3f8420cee051354b7aa462ebe1d7
Kittnz <frederik156@hotmail.com>
2017-03-15 23:43:49 +0100

    Remove anticheat option
    MIGRATIONS
    and revision

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,5 +1,5 @@
 #ifndef __REVISION_DATA_H__
 #define __REVISION_DATA_H__
- #define REVISION_HASH                      "426fbd782c575a346233"
- #define REVISION_DATE                      "2017-03-06 15:49:04 +0100"
+ #define REVISION_HASH                      "3bfd3fda2129fd8b1f81"
+ #define REVISION_DATE                      "2017-03-15 23:39:56 +0100"
 #endif // __REVISION_DATA_H__
